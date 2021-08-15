28cbf294e1d6c1e130dc6c4acee15d7f5eba1098
arcelioeperez <aepzgar@gmail.com>
2021-08-14 23:18:51 -0400

    ,

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 echo "Click the link to test the website"
 echo http://localhost:8080/view/ANewPage
-echo "Pres Ctrl+C to quit"
+echo "Press Ctrl+C to quit"
 cd gowiki && go build wiki.go && ./wiki
